57a8ec875c02f7c8928c4437e11f195e5d2b410d
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2024-09-12 21:52:51 +0200

    changed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>2024 Wypakuj Mnie Wszelkie Prawa Zastrzeżone</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
       </p>
+      <Link href="/privacy">RODO</Link>
     </div>
   )
 }
