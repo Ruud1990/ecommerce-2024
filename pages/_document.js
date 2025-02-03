fc10ea9c10a600f568ffae5ad1759d13567353fb
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2025-02-03 22:51:20 +0100

    changed meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Personalizowane upominki z drewna grawerowane i cięte laserem. Znajdź idealny prezent dla bliskiej osoby!" />
+<meta name="robots" content="index, follow" />
+<meta property="og:title" content="Wypakuj Mnie - Personalizowane Upominki" />
+<meta property="og:description" content="Unikalne prezenty z drewna na każdą okazję - chrzest, urodziny, ślub i wiele więcej." />
+<meta property="og:url" content="https://wypakuj-mnie.pl" />
+<meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
